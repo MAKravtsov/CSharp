850712f6fc991b9dc528429701fc4dc40a839892
MKravtsov <MKravtsov@croc.ru>
2021-04-01 20:09:28 +0300

    Добавлен новый проект для дальнейшей работы с WPF

---
+++ WPF/WpfAlive/WpfAlive/Phone.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAlive
+{
+    public class Phone: DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty;
+
+        public static readonly DependencyProperty PriceProperty;
+
+        static Phone()
+        {
+            var metadata = new FrameworkPropertyMetadata() { CoerceValueCallback = CoerceValueCallback};
+            TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(Phone));
+            PriceProperty = DependencyProperty.Register("Price", typeof(double), typeof(Phone), metadata, ValidateValueCallback);
+        }
+
+        private static object CoerceValueCallback(DependencyObject d, object basevalue)
+        {
+            var val = (double) basevalue;
+            return val > 1000 ? 1000 : val;
+        }
+
+        private static bool ValidateValueCallback(object value)
+        {
+            var val = (double) value;
+            return val >= 0;
+        }
+
+        public string Title {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public double Price {
+            get => (double)GetValue(PriceProperty);
+            set => SetValue(PriceProperty, value);
+        }
+    }
+}
