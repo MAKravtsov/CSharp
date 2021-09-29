638abd76f5dda9f3e86d4f1cd9a040b074a4c657
MKravtsov <MKravtsov@croc.ru>
2021-09-29 13:16:26 +0300

    Тренировка рекурсивной функции

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,14 +6,62 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class TreeNode
+    {
+        public IEnumerable<TreeNode> Children { get; set; } = new List<TreeNode>();
+
+        public IEnumerable<TreeNode> GetAllNodes()
+        {
+            var toReturn = new List<TreeNode> { this };
+
+            foreach (var node in Children) {
+                var nodes = node.GetAllNodes();
+                toReturn.AddRange(nodes);
+            }
+
+            return toReturn;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            /*
             var a = "A999".ToCharArray();
             Increment(a);
+            */
+
+            /*
+            int? b = null;
+            var c = Task.Factory.StartNew(() => A(out b)).Result;
+
+            int r = 4;
+            */
+            var t2 = new TreeNode();
+            var t1 = new TreeNode();
+
+            var expect = new List<TreeNode> { t1, t2 };
+
+            t1.Children = new List<TreeNode>{ t2 };
+
+            var result = t1.GetAllNodes().ToList();
+
+            bool flag = true;
+
+            for(int i = 0; i < expect.Count; i++) {
+                if(!ReferenceEquals(expect[i], result[i])) {
+
+                }
+            }
         }
 
+        
+        static bool A(out int? num)
+        {
+            num = 2;
+            throw new Exception();
+        }
         static void Increment(char[] referenceNumber)
         {
             if(referenceNumber == null || !referenceNumber.Any())
@@ -40,11 +88,13 @@ namespace ConsoleApp1
 
             var b = (decimal.Parse(a) + 1).ToString().ToCharArray();
 
+            /*
             if (b.Length > referenceNumber.Length) {
                 result.Add();
             } else {
                 result.AddRange(b);
             }
+            */
 
             referenceNumber = result.ToArray();
         }
